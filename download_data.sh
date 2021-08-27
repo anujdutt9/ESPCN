302838ab69d0522c1c9c5f8aaafd14a1d56c7001
anujdutt9 <anuj.dutt9@gmail.com>
2021-08-26 19:16:38 -0700

    added comments for steps

--- download_data.sh
+++ download_data.sh
@@ -1,11 +1,16 @@
-# Download DIV2K dataset
+# Download DIV2K Training and Validation datasets
 mkdir dataset
 
+echo "Downloading training and validation zip files."
 wget data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
 wget data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
 
+echo "Extracting training and validation zip files."
 unzip DIV2K_train_HR.zip -d ./dataset
 unzip DIV2K_valid_HR.zip -d ./dataset
 
+echo "Cleanup."
 rm DIV2K_train_HR.zip
 rm DIV2K_valid_HR.zip
+
+echo "Done."
